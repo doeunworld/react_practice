62335594a451a80e8f27cafbead5727dce3d01da
factory sunny <factorysunny@factoryui-iMac-2.local>
2022-11-14 10:11:34 +0900

    10시수정

--- my-react-app/src/main/index.js
+++ my-react-app/src/main/index.js
@@ -1,38 +1,66 @@
+import React from "react";
 import "./index.css";
 import axios from "axios";
 
 function MainPage() {
-  axios.get();
+  const [products, setProducts] = React.useState([]);
+  React.useEffect(function () {
+    axios
+      .get(
+        "https://341cf269-c712-4751-a587-2c7fd1b972ec.mock.pstmn.io/products"
+      )
+      .then(function (result) {
+        const products = result.data.products;
+        setProducts(products);
+        console.log(result);
+      })
+      .catch(function (error) {
+        console.error("에러 발생 : ", error);
+      });
+  }, []);
+
   return (
     <div>
-      <div className="header">
-        <img src="./images/wavetop.jpeg" alt="파도윗부분" />
-      </div>
-      <div className="mainContainer">
-        <h2 className="title">도은 마켓</h2>
-
-        <div className="mainWrapper">
-          <div id="product-list">
-            <div className="prouduct-card">
-              <div>
-                <img
-                  className="product-img"
-                  src="./images/products/keyboard1.jpg"
-                  alt="키보드 이미지"
-                ></img>
-              </div>
-              <div className="product-contents">
-                <span className="product-name">키보드</span>
-                <span className="product-price">10000원</span>
-                <div className="product-seller">도은</div>
-              </div>
-            </div>
-          </div>
+      <div id="header">
+        <div id="header-area">
+          <img src="images/icons/logo.png" alt="로고" />
         </div>
       </div>
-      <div className="footer">
-        <img src="./images/wavebottom.jpeg" alt="파도아랫부분" />
+      <div id="body">
+        <div id="banner">
+          <img src="images/wavetop.jpeg" alt="배경" />
+        </div>
+        <h1>판매되는 상품들</h1>
+        <div id="product-list">
+          {products &&
+            products.map(function (product, index) {
+              return (
+                <div className="product-card">
+                  <div>
+                    <img
+                      className="product-img"
+                      src={product.imageUrl}
+                      alt="상품이미지"
+                    />
+                  </div>
+                  <div className="product-contents">
+                    <span className="product-name">{product.name}</span>
+                    <span className="product-price">{product.price}원</span>
+                    <div className="product-seller">
+                      <img
+                        className="product-avatar"
+                        src="images/icons/avatar.png"
+                        alt="상품이미지"
+                      />
+                      <span>{product.seller}</span>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
       </div>
+      <div id="footer"></div>
     </div>
   );
 }
